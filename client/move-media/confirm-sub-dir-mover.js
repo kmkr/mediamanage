0460834cef7e74cda4f601e92f347116a94d4875
Kris-Mikael Krister <krismikael@protonmail.com>
2016-09-23 15:00:12 +0200

    Drop stream writing

--- client/move-media/confirm-sub-dir-mover.js
+++ client/move-media/confirm-sub-dir-mover.js
@@ -7,47 +7,45 @@ const Promise = require('bluebird');
 const logger = require('../vorpals/logger');
 const fileMover = require('../file-system/mover');
 
-module.exports = ({filePaths, destDirPath, vorpalInstance}) => (
-    new Promise((resolve, reject) => {
-        assert(filePaths && filePaths.constructor === Array, `File paths must be an array. Was ${filePaths}`);
+module.exports = ({filePaths, destDirPath, vorpalInstance}) => {
+    assert(filePaths && filePaths.constructor === Array, `File paths must be an array. Was ${filePaths}`);
 
-        logger.log('\n');
-        filePaths.forEach(filePath => {
-            logger.log(`Preparing to move ${chalk.yellow(filePath)}`);
-        });
+    logger.log('\n');
+    filePaths.forEach(filePath => {
+        logger.log(`Preparing to move ${chalk.yellow(filePath)}`);
+    });
 
-        let destinationDirAlternatives;
-        try {
-            destinationDirAlternatives = fs.readdirSync(destDirPath)
-                .filter(fileNameCandidate => (
-                    fs.statSync(path.join(destDirPath, fileNameCandidate)).isDirectory()
-                ));
-        } catch (err) {
-            if (err.code === 'ENOENT') {
-                logger.log(`Unable to move files - ${destDirPath} does not exist!`);
-                return resolve();
-            }
-
-            return reject(err);
+    let destinationDirAlternatives;
+    try {
+        destinationDirAlternatives = fs.readdirSync(destDirPath)
+            .filter(fileNameCandidate => (
+                fs.statSync(path.join(destDirPath, fileNameCandidate)).isDirectory()
+            ));
+    } catch (err) {
+        if (err.code === 'ENOENT') {
+            logger.log(`Unable to move files - ${destDirPath} does not exist!`);
+            return Promise.resolve();
         }
 
-        if (destinationDirAlternatives.length) {
-            // Add root dir as an option
-            destinationDirAlternatives.unshift('.');
+        return Promise.reject(err);
+    }
 
-            return vorpalInstance.activeCommand.prompt({
-                message: `Where do you want to move ${chalk.yellow(filePaths.length)} files?`,
-                name: 'moveDestination',
-                type: 'list',
-                choices: destinationDirAlternatives
-            }).then(({moveDestination}) => {
-                destDirPath = path.resolve(destDirPath, moveDestination);
-                return fileMover.moveAll({filePaths, destDirPath, vorpalInstance});
-            });
-        } else {
-            destDirPath = path.resolve(destDirPath);
+    if (destinationDirAlternatives.length) {
+        // Add root dir as an option
+        destinationDirAlternatives.unshift('.');
+
+        return vorpalInstance.activeCommand.prompt({
+            message: `Where do you want to move ${chalk.yellow(filePaths.length)} files?`,
+            name: 'moveDestination',
+            type: 'list',
+            choices: destinationDirAlternatives
+        }).then(({moveDestination}) => {
+            destDirPath = path.resolve(destDirPath, moveDestination);
             return fileMover.moveAll({filePaths, destDirPath, vorpalInstance});
-        }
+        });
+    } else {
+        destDirPath = path.resolve(destDirPath);
+        return fileMover.moveAll({filePaths, destDirPath, vorpalInstance});
+    }
 
-    })
-);
+};
