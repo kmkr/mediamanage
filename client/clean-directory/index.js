74b225d5b643aa0c8fa4f3c1693a922599652854
Kris-Mikael Krister <krismikael@protonmail.com>
2016-09-28 07:59:15 +0200

    Simplify by returning promise from prompt

--- client/clean-directory/index.js
+++ client/clean-directory/index.js
@@ -1,32 +1,27 @@
 const rimraf = require('rimraf');
-const Promise = require('bluebird');
 
 const fileFinder = require('../file-system/finder');
 
-module.exports = vorpalInstance => (
-    new Promise(resolve => {
-        const rootDir = process.cwd();
-        const recursive = true;
-        const filePaths = fileFinder.allFiles({dirPath: rootDir, recursive});
-        const videoFileNames = fileFinder.video({dirPath: rootDir, recursive});
-        const audioFileNames = fileFinder.audio({dirPath: rootDir, recursive});
+module.exports = vorpalInstance => {
+    const rootDir = process.cwd();
+    const recursive = true;
+    const filePaths = fileFinder.allFiles({ dirPath: rootDir, recursive });
+    const videoFileNames = fileFinder.video({ dirPath: rootDir, recursive });
+    const audioFileNames = fileFinder.audio({ dirPath: rootDir, recursive });
 
-        filePaths
-            .forEach(filePath => vorpalInstance.log(filePath));
-        const message = `${filePaths.length} file(s) left (${videoFileNames.length} video, ${audioFileNames.length} audio). Delete?`;
+    filePaths
+        .forEach(filePath => vorpalInstance.log(filePath));
+    const message = `${filePaths.length} file(s) left (${videoFileNames.length} video, ${audioFileNames.length} audio). Delete?`;
 
-        vorpalInstance.activeCommand.prompt({
-            message,
-            name: 'confirmDelete',
-            type: 'confirm',
-            default: false
-        }, function ({confirmDelete}) {
-            if (confirmDelete) {
-                rimraf.sync(rootDir);
-                vorpalInstance.log(`Removed ${filePaths.length} file(s) and containing dir`);
-            }
-            return resolve();
-        });
-    })
-);
-
+    return vorpalInstance.activeCommand.prompt({
+        message,
+        name: 'confirmDelete',
+        type: 'confirm',
+        default: false
+    }).then(({confirmDelete}) => {
+        if (confirmDelete) {
+            rimraf.sync(rootDir);
+            vorpalInstance.log(`Removed ${filePaths.length} file(s) and containing dir`);
+        }
+    });
+};
