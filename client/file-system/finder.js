5ee65ceca5c24b2ea65ef75f21fd1b52ab8fb2e5
Kris-Mikael Krister <krismikael@protonmail.com>
2017-02-06 07:37:37 +0100

    Add m2ts to accepted formats

--- client/file-system/finder.js
+++ client/file-system/finder.js
@@ -9,7 +9,7 @@ function isAudio(filePath) {
 }
 
 function isVideo(filePath) {
-    return filePath.match(/\.(mkv|mp4|avi|mpeg|iso|wmv)$/i);
+    return filePath.match(/\.(mkv|mp4|avi|mpeg|iso|wmv|m2ts)$/i);
 }
 
 function allFiles({ dirPath = process.cwd(), recursive = false } = {}) {
