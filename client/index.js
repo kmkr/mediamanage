b11ede366fa0163d2ff80961471578df2021b1d7
Kris-Mikael Krister <kris-mikael.krister@finn.no>
2017-06-22 09:15:51 +0200

    Use persistent history

--- client/index.js
+++ client/index.js
@@ -5,11 +5,15 @@ Promise.promisifyAll(require('mkdirp'));
 
 const singleFileVorpal = require('./vorpals/single-file-vorpal');
 const multiFileVorpal = require('./vorpals/multi-file-vorpal')(filePath => {
-    singleFileVorpal(filePath, showMultiFile).show();
+    const vorpalInstance = singleFileVorpal(filePath, showMultiFile);
+    vorpalInstance.history('mediamanage-single');
+    vorpalInstance.show();
 });
 
 function showMultiFile() {
     multiFileVorpal.show();
 }
+
+multiFileVorpal.history('mediamanage-multi');
 
 showMultiFile();
