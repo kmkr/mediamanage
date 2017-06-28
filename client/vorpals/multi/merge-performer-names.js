be52acb95c74e675e73acd6c0e3ff092e2de4612
Kris-Mikael Krister <krismikael@protonmail.com>
2017-06-28 07:39:39 +0200

    Return prompt to avoid exit

--- client/vorpals/multi/merge-performer-names.js
+++ client/vorpals/multi/merge-performer-names.js
@@ -6,60 +6,62 @@ const performerNames = require('../../performers/performer-name-list');
 const { flatten, unique } = require('../../helpers/array-helper');
 const existingMediaParser = require('../../helpers/existing-media-parser');
 
-module.exports = vorpal => {
-    const allVideos = finder.video({ recursive: true });
-    const performerNamesFromVideos = allVideos
-        .map(filePath => existingMediaParser.getPerformerNames(filePath))
-        .reduce(flatten, [])
-        .sort()
-        .filter(unique);
+module.exports = vorpal => (
+    new Promise(resolve => {
+        const allVideos = finder.video({ recursive: true });
+        const performerNamesFromVideos = allVideos
+            .map(filePath => existingMediaParser.getPerformerNames(filePath))
+            .reduce(flatten, [])
+            .sort()
+            .filter(unique);
 
-    vorpal.activeCommand.prompt({
-        message: 'Which names do you want to merge?',
-        type: 'checkbox',
-        name: 'mergeCandidates',
-        choices: performerNamesFromVideos
-    }, ({ mergeCandidates }) => {
-        if (mergeCandidates.length < 2) {
-            logger.log('Merge canceled');
-            return Promise.resolve();
-        }
-
-        return vorpal.activeCommand.prompt({
-            message: 'Which of the names do you want to use?',
-            type: 'list',
-            name: 'nameToUse',
-            choices: mergeCandidates
-        }, ({ nameToUse }) => {
-            if (!nameToUse) {
+        vorpal.activeCommand.prompt({
+            message: 'Which names do you want to merge?',
+            type: 'checkbox',
+            name: 'mergeCandidates',
+            choices: performerNamesFromVideos
+        }, ({ mergeCandidates }) => {
+            if (mergeCandidates.length < 2) {
                 logger.log('Merge canceled');
-                return Promise.resolve();
+                return resolve();
             }
 
-            const namesToMerge = mergeCandidates.filter(mergeCandidate => mergeCandidate !== nameToUse);
+            vorpal.activeCommand.prompt({
+                message: 'Which of the names do you want to use?',
+                type: 'list',
+                name: 'nameToUse',
+                choices: mergeCandidates
+            }, ({ nameToUse }) => {
+                if (!nameToUse) {
+                    logger.log('Merge canceled');
+                    return resolve();
+                }
 
-            const matchingVideos = finder.video({ recursive: true })
-                .filter(filePath => {
-                    const fileName = path.parse(filePath).name;
-                    return namesToMerge.some(nameToMerge => fileName.includes(nameToMerge));
+                const namesToMerge = mergeCandidates.filter(mergeCandidate => mergeCandidate !== nameToUse);
+
+                const matchingVideos = finder.video({ recursive: true })
+                    .filter(filePath => {
+                        const fileName = path.parse(filePath).name;
+                        return namesToMerge.some(nameToMerge => fileName.includes(nameToMerge));
+                    });
+
+                if (!matchingVideos.length) {
+                    logger.log('No matching videos found. Merge canceled.');
+                    return resolve();
+                }
+
+                matchingVideos.map(filePath => {
+                    const { ext, dir, name: fileName } = path.parse(filePath);
+                    const newFileName = namesToMerge.reduce((prevVal, curVal) => (
+                        prevVal.replace(new RegExp(curVal, 'g'), nameToUse)
+                    ), fileName);
+                    logger.log(`mv "${filePath}" "${dir}${path.sep}${newFileName}${ext}"`);
                 });
 
-            if (!matchingVideos.length) {
-                logger.log('No matching videos found. Merge canceled.');
-                return Promise.resolve();
-            }
+                performerNames.remove(namesToMerge);
 
-            matchingVideos.map(filePath => {
-                const { ext, dir, name: fileName } = path.parse(filePath);
-                const newFileName = namesToMerge.reduce((prevVal, curVal) => (
-                    prevVal.replace(new RegExp(curVal, 'g'), nameToUse)
-                ), fileName);
-                logger.log(`mv "${filePath}" "${dir}${path.sep}${newFileName}${ext}"`);
+                return resolve();
             });
-
-            performerNames.remove(namesToMerge);
-
-            return Promise.resolve();
         });
-    });
-};
+    })
+);
