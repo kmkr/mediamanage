d8fafeca9428e743ede16d5f89deaffc7b2ed463
Kris-Mikael Krister <krismikael@protonmail.com>
2017-09-23 16:06:01 +0200

    Normalize names

--- client/vorpals/single/autonames.js
+++ client/vorpals/single/autonames.js
@@ -3,10 +3,14 @@ const performerNameList = require('../../performers/performer-name-list');
 const currentFilePathStore = require('./current-file-path-store');
 const categoriesAndPerformerNamesHandler = require('./categories-and-performer-names-handler');
 
+function normalize(name) {
+    return name.toLowerCase().replace(/[^a-z]/g, '');
+}
+
 module.exports = filePath => (
     new Promise(resolve => {
         const { name } = path.parse(filePath);
-        const namesInFileTitle = performerNameList.list().filter(performerName => name.includes(performerName));
+        const namesInFileTitle = performerNameList.list().filter(performerName => normalize(name).includes(normalize(performerName)));
         const newPath = categoriesAndPerformerNamesHandler(namesInFileTitle, filePath);
         currentFilePathStore.set(newPath);
 
