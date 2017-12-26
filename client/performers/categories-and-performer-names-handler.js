d88cc2e40a5d1e5504c89d24612471d8f06deddd
Kris-Mikael Krister <krismikael@protonmail.com>
2017-12-26 21:46:19 +0100

    Do not add 'd' as a perf name

--- client/performers/categories-and-performer-names-handler.js
+++ client/performers/categories-and-performer-names-handler.js
@@ -2,9 +2,13 @@ const config = require('../config.json')
 const fileRenamer = require('../file-system/renamer')
 const performerNameList = require('./performer-name-list')
 
+const MIN_LENGTH_PERFORMER_NAME = 2
+
 module.exports = (performerNamesAndCategories, destFilePath) => {
   const categories = performerNamesAndCategories.filter(entry => config.categories.includes(entry))
-  const performerNames = performerNamesAndCategories.filter(entry => !categories.includes(entry))
+  const performerNames = performerNamesAndCategories
+    .filter(entry => !categories.includes(entry))
+    .filter(entry => entry.length >= MIN_LENGTH_PERFORMER_NAME)
 
   let filePath = destFilePath
 
