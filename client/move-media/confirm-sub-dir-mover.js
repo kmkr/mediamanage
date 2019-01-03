4ca658b0b8eaa44ebe096f02312047d275d3c8cf
Kris-Mikael Krister <krismikael@protonmail.com>
2019-01-03 13:38:17 +0100

    Default to prev choice when moving files

--- client/move-media/confirm-sub-dir-mover.js
+++ client/move-media/confirm-sub-dir-mover.js
@@ -8,6 +8,8 @@ const promptCreateFolder = require('./prompt-create-folder')
 const printPathsService = require('../helpers/print-paths-service')
 const removeCurrentWd = require('../helpers/remove-current-wd')
 
+let previousChoice
+
 module.exports = ({ filePaths, destDirPath, vorpalInstance }) => {
   assert(filePaths && filePaths.constructor === Array, `File paths must be an array. Was ${filePaths}`)
 
@@ -27,8 +29,10 @@ module.exports = ({ filePaths, destDirPath, vorpalInstance }) => {
             const ABORT = 'Abort move'
             destinationDirAlternatives.unshift(ABORT)
 
+            const defaultChoice = destinationDirAlternatives.includes(previousChoice) ? previousChoice : ABORT
             return vorpalInstance.activeCommand.prompt({
               message: `Where do you want to move the above ${chalk.yellow(filePaths.length)} files?`,
+              default: defaultChoice,
               name: 'moveDestination',
               type: 'list',
               choices: destinationDirAlternatives
@@ -36,6 +40,7 @@ module.exports = ({ filePaths, destDirPath, vorpalInstance }) => {
               if (!moveDestination || moveDestination === ABORT) {
                 return
               }
+              previousChoice = moveDestination
               destDirPath = path.resolve(destDirPath, moveDestination)
               return fileMover.moveAll({ filePaths, destDirPath, vorpalInstance })
             })
